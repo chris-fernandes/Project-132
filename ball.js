41f1097b889e8cd3d16844bc69c587ce1ee26af1
chris-fernandes <83762983+chris-fernandes@users.noreply.github.com>
2022-04-07 15:36:13 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,30 @@
+Status = "";
+ball_image = "";
+
+function preload(){
+    ball_image = loadImage("Ball.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(350,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(ball_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(ball_image,0,0,350,350);
+}
